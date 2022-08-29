452e0ec9ed7abbe9dfd2b61275560dd8a805e22d
Poseyd29 <poseyd29@gmail.com>
2022-08-29 03:52:35 -0400

    foundation
    database has been created and connected. server is running fine. html and css template has been added. now to work on functionality

--- server.js
+++ server.js
@@ -7,5 +7,10 @@ require('dotenv').config({ path: './config/.env' })
 // Connect to database on server start
 connectDB()
 
+//Set Middleware
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
 //Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
